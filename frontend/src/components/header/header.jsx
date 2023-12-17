b0b8dbf3664736356820f41b9d89bfb4e0ffe49b
sadranamavar <amirsadra.namavar@gmail.com>
2023-12-17 16:25:24 +0330

    make header

---
+++ frontend/src/components/header/header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return ( 
+    <div className="text-center bg-2 rounded-bottom-3 p-2 mx-dsa1 shadow fs-4 text-4">
+        💀 Do or Die 💀
+    </div> );
+}
+ 
+export default Header;
