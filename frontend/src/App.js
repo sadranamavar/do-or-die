3e493a46cdf274fbd15372ebafa36c4b25c4d0da
sadranamavar <amirsadra.namavar@gmail.com>
2023-12-17 16:24:57 +0330

    add routes system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/home/home";
+import Layout from "./components/layout/layout";
+import List from "./components/list/list";
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home/>} />
+            <Route path="list" element={<List />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
