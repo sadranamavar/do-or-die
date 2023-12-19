b6ce8c79dcdbea8d9dcaba0909182ec5980539af
sadranamavar <amirsadra.namavar@gmail.com>
2023-12-19 12:05:36 +0330

    fix date label issus in addTask page

--- frontend/src/components/addTask/time/time.jsx
+++ frontend/src/components/addTask/time/time.jsx
@@ -3,7 +3,7 @@ import Label from "../label/label";
 const Time = ({props}) => {
     return ( <>
     <div className="bg-3 fs-2 p-1 pt-2 shadow row rounded-4 mx-1 mt-2 mt-3">
-        <Label title={"time"} />
+        <Label title={"Time"} />
         <input  type="time" 
         className="input-d  ps-3 col-12 text-1" 
         onChange={e => props[1](e.target.value)}
