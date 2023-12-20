51226d4697ce0a29b3ff6dd90a7b72ad931fb8f8
sadranamavar <amirsadra.namavar@gmail.com>
2023-12-20 19:38:21 +0330

    make all color style in new file

--- frontend/src/components/layout/layout.jsx
+++ frontend/src/components/layout/layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../header/header";
+import Style from "./style";
 
 const Layout = () => {
   return (
     <div className="bg-1 vh-100 main-dev">
+      <Style />
       <Header />
       <Outlet />
     </div>
