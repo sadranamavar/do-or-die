f705461d606f4f39c29974cf3ab56f750d1f2dc2
sadranamavar <amirsadra.namavar@gmail.com>
2023-12-21 00:15:27 +0330

    add send notification system

--- frontend/src/components/list/tasks/tasks.jsx
+++ frontend/src/components/list/tasks/tasks.jsx
@@ -1,10 +1,11 @@
 import toast from "react-hot-toast";
 import Task from "./task/task";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Tasks = () => {
   const tasks = [];
-
+  const navigate = useNavigate()
   const isNumber = (value) => {
     return !isNaN(parseFloat(value)) && isFinite(value);
   };
@@ -15,7 +16,7 @@ const Tasks = () => {
   };
   const getNow = () => {
     const date = new Date();
-    return `${date.getHours()}:${date.getMinutes()}`;
+    return `${date.getHours()==0 ? "00":date.getHours()}:${date.getMinutes()}`;
   };
 
   for (let i = 0; i < localStorage.length; i++) {
@@ -30,9 +31,20 @@ const Tasks = () => {
       ) {
         localStorage.removeItem(localStorage.key(i));
       }
-      item.tm = false
+      item.tm = false;
       tasks.push(item);
     }
+  }
+  const getNotificationPermission = () => {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        // toast.success("now do-or-die can call you with a notification")
+      } else {
+        toast.error("do-or-die can't call you with a notification")
+        
+      }
+    });
+    
   }
   const toastManager = () => {
     for (let i = tasks.length - 1; i >= 0; i--) {
@@ -40,19 +52,28 @@ const Tasks = () => {
         new Date(tasks[i].date).toDateString() === getToday().toDateString() &&
         tasks[i].time.slice(0, 2) === getNow().slice(0, 2) &&
         Number(tasks[i].time.slice(3, 5)) >= Number(getNow().slice(3, 5)) &&
-        Number(tasks[i].time.slice(3, 5)) <= Number(getNow().slice(3, 5)) + 1 &&
-        !tasks[i].status && 
-        !tasks[i].tm 
+        Number(tasks[i].time.slice(3, 5)) <= Number(getNow().slice(3, 5)) + 1 && 
+        !tasks[i].status &&
+        !tasks[i].tm
       ) {
-        tasks[i].tm = true
+        tasks[i].tm = true;
         toast(tasks[i].title);
+        const notification = new Notification("New Message", {
+          body: "You have a new message from John",
+          icon: "https://example.com/icon.png",
+          tag: "new-message",
+          data: { sender: "John" },
+          requireInteraction: true
+        });
+        notification.onclick=()=>(navigate(`add/${tasks[i].id}`))
       }
-      setTimeout(toastManager,10000)
+      setTimeout(toastManager, 10000);
     }
   };
   useEffect(() => {
     toastManager();
-  },);
+    getNotificationPermission()
+  });
   return (
     <>
       {tasks.map((item) => (
