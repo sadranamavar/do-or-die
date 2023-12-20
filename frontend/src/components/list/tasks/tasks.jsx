c9cd98d4f08f77d58fa3466141d0ce055c93e662
sadranamavar <amirsadra.namavar@gmail.com>
2023-12-20 18:02:15 +0330

    remove tasks after down and end task day

--- frontend/src/components/list/tasks/tasks.jsx
+++ frontend/src/components/list/tasks/tasks.jsx
@@ -1,16 +1,26 @@
 import Task from "./task/task";
 
 const Tasks = () => {
+  const getNow = () => {
+    const date = new Date()
+    return(new Date(date.toDateString()))
+  }
   const tasks = [];
   for (let i = 0; i < localStorage.length; i++) {
     if (localStorage.key(i) === "last-id") {
       continue;
+    } 
+    else {
+      const item = JSON.parse(localStorage.getItem(localStorage.key(i)));
+      console.log(item.date ?(( new Date(item.date))-getNow()): "")
+      if (item.status && ((item.date ?(( new Date(item.date))-getNow()): false)<0)){
+        localStorage.removeItem(localStorage.key(i))
+      }
+      // console.log(getNow())
+      tasks.push(item);
     }
-    else{
-    const item = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    tasks.push(item);}
   }
-
+  console.log(tasks);
   return (
     <>
       {tasks.map((item) => (
