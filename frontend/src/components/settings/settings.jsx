92a616ea2dc4d8c2bbea381e9a7d6413a385f0b0
sadranamavar <amirsadra.namavar@gmail.com>
2023-12-26 22:26:42 +0330

    fix settings pages bug

--- frontend/src/components/settings/settings.jsx
+++ frontend/src/components/settings/settings.jsx
@@ -1,11 +1,9 @@
-import CategorySettings from "./categorySettings/categorySettings";
 import ColorSetting from "./colorSettings/colorSettings";
 
 const Settings = () => {
     return ( <>
     <ColorSetting />
     <hr className="border border-2 shadow border-light mx-5 my-4 rounded-5"/>
-    <CategorySettings />
     </> );
 }
  
