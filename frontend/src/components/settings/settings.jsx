e0a989799fbed5b0ecbd18836b6918488cec0cdf
sadranamavar <amirsadra.namavar@gmail.com>
2023-12-26 22:26:04 +0330

    add colors settings pages

--- frontend/src/components/settings/settings.jsx
+++ frontend/src/components/settings/settings.jsx
@@ -1,5 +1,12 @@
+import CategorySettings from "./categorySettings/categorySettings";
+import ColorSetting from "./colorSettings/colorSettings";
+
 const Settings = () => {
-    return ( <>settings</> );
+    return ( <>
+    <ColorSetting />
+    <hr className="border border-2 shadow border-light mx-5 my-4 rounded-5"/>
+    <CategorySettings />
+    </> );
 }
  
 export default Settings;
